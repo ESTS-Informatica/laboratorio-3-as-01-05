1e9e9ef33b50164e54391ef8e637f3c0f2fd9cf7
202200189 <202200189@estudantes.ips.pt>
2024-04-04 10:33:01 +0100

    nivel 1

---
+++ GroundTransportation.java
@@ -0,0 +1,34 @@
+
+/**
+ * Escreva uma descrição da classe GroundTransportation aqui.
+ * 
+ * @author (seu nome) 
+ * @version (um número da versão ou uma data)
+ */
+public class GroundTransportation extends Transport
+{
+    private String licensePlate;
+    private static int fees = 3;
+    
+    
+    /**
+     * Construtor para objetos da classe GroundTransportation
+     */
+    public GroundTransportation()
+    {
+        // inicializa variáveis de instância
+        x = 0;
+    }
+
+    /**
+     * Um exemplo de um método - substitua este comentário pelo seu próprio
+     * 
+     * @param  y   um exemplo de um parâmetro de método
+     * @return     a soma de x e y 
+     */
+    public int sampleMethod(int y)
+    {
+        // escreva seu código aqui
+        return x + y;
+    }
+}
